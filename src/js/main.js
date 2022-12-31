2850040bb44fc408759037b9523ae7afb9a9a3d4
ArkoAnnuk <arkoannuk+prog@gmail.com>
2022-12-31 20:07:00 +0200

    Migrate

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+// Import gsap animations
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+
+// Jumbotron Animations
+// gsap.fromTo(".jumboLogoGsap", { y: 100, opacity: 0, }, { scrollTrigger: ".jumboLogoGsap", y: 0, opacity: 1, delay: 0, duration: 1, ease: "Power3.easeInOut" });
+// gsap.fromTo(".jumboTextGsap", { y: 100, opacity: 0, }, { scrollTrigger: ".jumboTextGsap", y: 0, opacity: 1, delay: 0.1, duration: ease: "Power3.easeInOut" });
+
+// Services Animations
